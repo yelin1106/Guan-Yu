be2f73381889cfdc8eca28a1be0489f1889d3f0c
yelin <yelin1106@naver.com>
2020-12-24 10:36:25 +0000

    week8 push

--- programmers/main.py
+++ programmers/main.py
@@ -55,4 +55,5 @@
 ####import week8.make_big_num
 #import week8.carpet
 #import week8.phone_number_list
-import week8.find_prime
+#import week8.find_prime
+import week8.rescue_boat
---
+++ programmers/week8/rescue_boat.py
@@ -0,0 +1,32 @@
+# 구명보트
+
+#s내가 짠 코드 중에 제일 빠른 코드 (효율성 통과 못함)
+# def solution(people, limit):
+#   answer = 0
+#   people.sort(reverse=True)
+#   while people:
+#     remain=limit-people[0]
+#     people.pop(0)
+#     for p in people:
+#       if p<=remain:
+#         people.remove(p)
+#         break
+#     answer+=1
+#   return answer
+
+# 질문하기 참고하고 짠코드 (미쳐벌인 속도)
+def solution(people, limit):
+  answer = 0
+  people.sort(reverse=True)
+  idx1=0
+  idx2=len(people)-1
+  while idx1<=idx2:
+    if people[idx1]+people[idx2]<=limit:
+      idx2-=1
+    idx1+=1
+    answer+=1
+  return answer
+
+people=[70, 50, 80, 50]
+limit=100
+print(solution(people,limit))
