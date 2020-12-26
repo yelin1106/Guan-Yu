ca97a5f46b277653a678d6b915ef2f8dc4d3b05c
yelin <yelin1106@naver.com>
2020-12-26 10:54:32 +0000

    week8 push

--- programmers/main.py
+++ programmers/main.py
@@ -51,8 +51,8 @@
 #import week7.H_Index
 
 
-import week8.joystick
-####import week8.make_big_num
+#import week8.joystick
+import week8.make_big_num
 #import week8.carpet
 #import week8.phone_number_list
 #import week8.find_prime
--- programmers/week8/make_big_num.py
+++ programmers/week8/make_big_num.py
@@ -1,23 +1,37 @@
 # 큰 수 만들기
 
+#시간초과 코드
+# def solution(number, k):
+#   nums=list(number)
+#   cnt=idx=0
+#   while idx<len(nums) and cnt!=k:
+#     if idx<len(nums)-1 and nums[idx]<nums[idx+1]:
+#       print(nums.pop(idx))
+#       cnt+=1
+#       if idx!=0 : idx-=1
+#     else:
+#       idx+=1
+#   cnt+=k-cnt
+#   for _ in range(k-cnt):
+#     nums.pop()
+    
+#   print(nums)
+#   answer = "".join(nums)
+#   return answer
+
 def solution(number, k):
-  nums=list(number)
-  cnt=0
-  while k!=cnt:
-    for idx,n in enumerate(nums):
-      if idx<len(nums)-1 and n<nums[idx+1]:
-        cnt+=1
-        nums.pop(idx)
-        break
-    else:
-      cnt+=k-cnt
-      for _ in range(k-cnt):
-        nums.pop()
-  print(nums)
-  answer = "".join(nums)
+  stack=[number[0]]
+  for num in number[1:]:
+    while len(stack)>0 and k>0 and stack[-1]<num:
+      k-=1
+      stack.pop()
+    stack.append(num)
+  answer="".join(stack)
+  while k>0:
+    stack.pop()
   return answer
 
-
 number="4177252841"
-k=4
+number="1111"
+k=2
 print(solution(number,k))
